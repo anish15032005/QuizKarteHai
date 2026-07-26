736e1f40e8dbe4267044f319bdc69dbf269e08db
Anish Mansuri <anisharyan055@gmail.com>
2026-07-26 08:42:01 +0530

    updated logging.py

--- backend/app/core/logging.py
+++ backend/app/core/logging.py
@@ -2,3 +2,13 @@ import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+)
+
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
