790e0a4451f448da9355f9d5af220aa8e2d97338
Anish Mansuri <anisharyan055@gmail.com>
2026-07-26 08:58:56 +0530

    updated logging.py

--- backend/app/core/logging.py
+++ backend/app/core/logging.py
@@ -1,12 +1,21 @@
 import logging
+from pathlib import Path
 
 logger = logging.getLogger("quizkartehai")
 logger.setLevel(logging.INFO)
+BASE_DIR = Path(__file__).resolve().parents[2]
+LOG_DIR = BASE_DIR / "logs"
+LOG_FILE = LOG_DIR / "application.log"
+# LOG_FILE = LOG_DIR / "application.log"
 
 # Prevent duplicate handlers
 if not logger.handlers:
 
     console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(
+    LOG_FILE,
+    encoding="utf-8"
+)
 
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s"
@@ -15,3 +24,6 @@ if not logger.handlers:
     console_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+    
+    
