e1c2be663c2a2f1411b50d567289960213de2c10
Anish Mansuri <anisharyan055@gmail.com>
2026-07-26 23:11:28 +0530

    updated user.py

---
+++ backend/app/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from app.database.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
