3dd435aa00300281ab6f3b96b0ad745ccadd4421
Anish Mansuri <anisharyan055@gmail.com>
2026-07-27 14:22:10 +0530

    defined user.py

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -2,10 +2,26 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from app.database.session import get_db
+from app.models.user import User
+from app.schemas.user import UserCreate
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.get("/")
 def get_users(db: Session = Depends(get_db)):
-    return {"message": "Database connection successful!"}
+    return db.query(User).all()
+
+
+@router.post("/")
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    new_user = User(
+        name=user.name,
+        email=user.email,
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
