d6685f1f5fcc316a99933a5c8e831120efe3a8fa
Anish Mansuri <anisharyan055@gmail.com>
2026-07-28 10:11:29 +0530

    updated user_services.py

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,10 +1,22 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
 
 from app.models.user import User
 from app.schemas.user import UserCreate
 
 
 def create_user_service(db: Session, user: UserCreate):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email already registered.",
+        )
+
     new_user = User(
         name=user.name,
         email=user.email,
