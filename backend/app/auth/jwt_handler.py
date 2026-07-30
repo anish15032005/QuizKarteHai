30ad49531675be888a7e6c128df11bde69e92482
Anish Mansuri <anisharyan055@gmail.com>
2026-07-30 11:25:49 +0530

    עדכן משהו

---
+++ backend/app/auth/jwt_handler.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta, timezone
+
+from jose import jwt
+
+SECRET_KEY = "your-super-secret-key-change-this-later"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(
+        to_encode,
+        SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+
+
+def verify_access_token(token: str):
+    return jwt.decode(
+        token,
+        SECRET_KEY,
+        algorithms=[ALGORITHM],
+    )
