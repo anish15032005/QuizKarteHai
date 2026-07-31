8dcf746d065ead5f429b20eed484545acb7bb3f6
Anish Mansuri <anisharyan055@gmail.com>
2026-08-01 00:04:13 +0530

    created ai_service.py

---
+++ backend/app/services/ai_service.py
@@ -0,0 +1,23 @@
+from google import genai
+
+from app.core.config import settings
+from app.prompts.mcq_prompt import MCQ_PROMPT
+
+client = genai.Client(
+    api_key=settings.GOOGLE_API_KEY
+)
+
+
+def generate_mcqs(text: str, num_questions: int = 5):
+
+    prompt = MCQ_PROMPT.format(
+        text=text,
+        num_questions=num_questions,
+    )
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+    )
+
+    return response.text
