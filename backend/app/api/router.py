5e73216f750e2bcc36647ced3a547dec02a9657e
Anish Mansuri <anisharyan055@gmail.com>
2026-07-31 23:00:24 +0530

    Что-то обновил

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 # Import the route modules and reference their `router` attributes
 from app.api.routes import health
 from app.api.routes import user
+from app.api.routes import auth
 
 api_router = APIRouter()
 
 api_router.include_router(health.router)
 api_router.include_router(user.router)
+api_router.include_router(auth.router)
