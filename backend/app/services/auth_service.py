e1d53433364ffd63427b8cf6ba1e34c6d8c04d6e
Anish Mansuri <anisharyan055@gmail.com>
2026-08-02 14:02:42 +0530

    <->

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -1,21 +1,18 @@
-from typing import cast
-
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
-from app.auth.hashing import verify_password
-from app.auth.jwt_handler import create_access_token
 from app.models.user import User
 from app.schemas.user import UserLogin
+from app.auth.hashing import verify_password
 
 
-def login_service(db: Session, user: UserLogin):
+def login_user_service(db: Session, user: UserLogin):
 
-    db_user = db.query(User).filter(
+    existing_user = db.query(User).filter(
         User.email == user.email
     ).first()
 
-    if not db_user:
+    if not existing_user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid email or password",
@@ -23,18 +20,11 @@ def login_service(db: Session, user: UserLogin):
 
     if not verify_password(
         user.password,
-        cast(str, db_user.hashed_password),
+        str(existing_user.password),
     ):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid email or password",
         )
 
-    token = create_access_token(
-        {"sub": db_user.email}
-    )
-
-    return {
-        "access_token": token,
-        "token_type": "bearer",
-    }
+    return existing_user
